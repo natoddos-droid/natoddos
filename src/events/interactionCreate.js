0eb90d69108c9f2907ef9646636c15136a12c6d1
natoddos-droid <kontaktklanmcf@gmail.com>
2026-04-13 20:37:20 +0200

    Create interactionCreate.js

---
+++ src/events/interactionCreate.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'interactionCreate',
+    async execute(interaction) {
+        if (!interaction.isChatInputCommand()) return;
+
+        const command = interaction.client.commands.get(interaction.commandName);
+
+        if (!command) return;
+
+        try {
+            await command.execute(interaction);
+        } catch (error) {
+            console.error(error);
+            await interaction.reply({ content: 'Wystąpił błąd podczas wykonywania komendy!', ephemeral: true });
+        }
+    },
+};
