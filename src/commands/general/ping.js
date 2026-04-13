9b5a66d9042c01c51166eff4c746ca5810fcf2d4
natoddos-droid <kontaktklanmcf@gmail.com>
2026-04-13 20:40:29 +0200

    Update ping.js

--- src/commands/general/ping.js
+++ src/commands/general/ping.js
@@ -1,1 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Odpowiada pong!'),
+
+    async execute(interaction) {
+        await interaction.reply('Pong!');
+    },
+};
