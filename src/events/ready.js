6905d591eaa3b88f91e44eea7f9eb0196dc7c886
natoddos-droid <kontaktklanmcf@gmail.com>
2026-04-13 20:44:53 +0200

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,7 +1,24 @@
+const { REST, Routes } = require('discord.js');
+require('dotenv').config();
+
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client) {
+    async execute(client) {
         console.log(`Zalogowano jako ${client.user.tag}`);
+
+        const commands = client.commands.map(cmd => cmd.data.toJSON());
+        const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+        try {
+            console.log('Rejestrowanie komend...');
+            await rest.put(
+                Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+                { body: commands }
+            );
+            console.log('Komendy zarejestrowane!');
+        } catch (error) {
+            console.error(error);
+        }
     },
 };
