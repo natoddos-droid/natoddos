776f41cfda2f1c64899c9ee78f5a8b6b90efcb4a
natoddos-droid <kontaktklanmcf@gmail.com>
2026-04-13 21:18:35 +0200

    Update messageCreate.js

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,18 +1,13 @@
 module.exports = {
     name: 'messageCreate',
     async execute(message) {
-
-        // Ignoruj boty
         if (message.author.bot) return;
 
-        // Treść wiadomości
         const content = message.content.toLowerCase();
 
-        // Jeśli wiadomość zaczyna się od +rep
         if (content.startsWith('+rep')) {
             try {
-                // Reakcja customową emotką
-                await message.react('1493328405129592842');
+                await message.react('<:NATO:1493328114011603055>');
             } catch (err) {
                 console.error('Nie mogę dodać reakcji:', err);
             }
