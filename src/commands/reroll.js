48090ca8ebde5fc262d5331fa00317b627b94050
natoddos-droid <kontaktklanmcf@gmail.com>
2026-04-15 21:35:28 +0200

    Update reroll command to use giveaway ID

--- src/commands/reroll.js
+++ src/commands/reroll.js
@@ -4,23 +4,35 @@ const fs = require('fs');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('reroll')
-        .setDescription('Losuje nowych zwycięzców giveaway'),
+        .setDescription('Reroll giveaway po ID')
+        .addStringOption(opt =>
+            opt.setName('id')
+                .setDescription('ID wiadomości giveaway')
+                .setRequired(true)
+        ),
 
     async execute(interaction) {
 
-        const data = JSON.parse(fs.readFileSync('./giveawayData.json', 'utf8'));
+        const id = interaction.options.getString('id');
+        const db = JSON.parse(fs.readFileSync('./giveaways.json', 'utf8'));
 
-        if (!data.participants.length) {
+        if (!db.giveaways[id]) {
+            return interaction.reply({ content: 'Nie znaleziono giveaway o tym ID!', ephemeral: true });
+        }
+
+        const g = db.giveaways[id];
+
+        if (!g.participants.length) {
             return interaction.reply({ content: 'Brak uczestników!', ephemeral: true });
         }
 
         let winnersList = [];
 
-        for (let i = 0; i < data.winners; i++) {
-            const winner = data.participants[Math.floor(Math.random() * data.participants.length)];
+        for (let i = 0; i < g.winners; i++) {
+            const winner = g.participants[Math.floor(Math.random() * g.participants.length)];
             winnersList.push(`<@${winner}>`);
         }
 
-        return interaction.reply(`🎉 **Nowi zwycięzcy:**\n${winnersList.join('\n')}`);
+        return interaction.reply(`🎉 **Nowi zwycięzcy giveaway ${id}:**\n${winnersList.join('\n')}`);
     }
 };
